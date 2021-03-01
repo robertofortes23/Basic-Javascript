8a759259717d3003f231f66daef22a3420ccc9be
Roberto Fortes <robertbernd23@gmail.com>
2021-03-01 18:02:57 -0300

    Operaçao Ternaria

---
+++ operacao_ternaria/index.js
@@ -0,0 +1,10 @@
+// ? :
+
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário Normal';
+console.log(nivelUsuario);
+
+const corUsuario = null;
+const corPadrao = corUsuario || "Azul";
+console.log(corPadrao);
+
