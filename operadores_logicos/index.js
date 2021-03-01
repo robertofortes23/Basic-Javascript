39c8c85fbbd43bc770077e7829b95b9f90aecdac
Roberto Fortes <robertbernd23@gmail.com>
2021-03-01 04:15:21 -0300

    Operadores Logicos

---
+++ operadores_logicos/index.js
@@ -0,0 +1,19 @@
+/*
+Operadores Lógicos
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+
+//Todas precisam ser verdadeiras para retornar verdadeiro.
+//Para falso uma precisa retornar falso.
+const expressaoAnd = true && true && false && true; //false
+const expressaoAnd2 = true && true //true
+
+//Basta que uma retorne verdadeiro para retornar verdadeiro.
+//Para falso todas precisam retornar falso
+const expressaoOr = false || false // false
+const expressaoOr = false || true // true
+
+
+
