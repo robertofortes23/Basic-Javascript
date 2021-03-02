90b5cc37165828f4cc321158660149ea048e2e0b
Roberto Fortes <robertbernd23@gmail.com>
2021-03-02 05:21:02 -0300

    Switch/Case

--- switch_case/index.js
+++ switch_case/index.js
@@ -1,7 +1,36 @@
 const data = new Date('1987-04-21 00:00:00');
 let diaSemana = data.getDay();
-let diaSemanaTexto;
-
 
 
 console.log(diaSemana);
+
+function getDayText(diaSemana){
+    let diaSemanaTexto;
+
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo'
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sábado';
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = '';
+            return diaSemanaTexto;
+    }
+}
