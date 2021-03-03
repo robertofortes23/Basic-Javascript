2474d1814e518c72a174cf10b00be07e8f7e7a98
Roberto Fortes <robertbernd23@gmail.com>
2021-03-02 21:38:29 -0300

    for classico

---
+++ for_classico/index.js
@@ -0,0 +1,13 @@
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i <= 20; i += 10) {
+    console.log(i);
+}
+
+const frutas = ['Maça', 'Pêra', 'Uva']; 
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`índice ${i}`,frutas[i]);
+}
